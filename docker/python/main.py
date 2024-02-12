af8d62769cc12c899a5b96e494afa8137b040b70
Nalvac <vacna1997@gmail.com>
2024-02-13 00:01:01 +0100

    Quelques refacto et correction Ajout du Newman(502 Gateway error à corrigé) et de l'nginx

--- docker/python/main.py
+++ docker/python/main.py
@@ -1,58 +1,74 @@
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
 import json
 import mysql.connector
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/health':
-            self.handle_health()
-        elif self.path == '/get':
-            self.handle_get()
+        if self.path == '/get':
+            response = self.get_iterator_status()
+        elif self.path == '/health':
+            response = self.health_status()
         else:
-            self.send_error(404)
+            super().do_GET()
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(bytes(response, 'utf-8'))
 
     def do_POST(self):
         if self.path == '/add':
-            self.handle_add()
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            response = self.add_iterator(json.loads(post_data.decode('utf-8')))
         else:
-            self.send_error(404)
+            super().do_POST()
 
-    def handle_health(self):
-        self.send_json_response(200, {"status": "OK"})
-
-    def handle_get(self):
-        with self.get_db_connection() as cnx:
-            cursor = cnx.cursor()
-            query = ("SELECT * FROM iterator")
-            cursor.execute(query)
-            iterators = [{"state": state} for state in cursor]
-            self.send_json_response(200, {"status": "ok", "iterators": iterators})
-
-    def handle_add(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        data = json.loads(post_data)
-        value = data['value']
-        with self.get_db_connection() as cnx:
-            cursor = cnx.cursor()
-            query = ("INSERT INTO iterator (state) VALUES (%s)")
-            cursor.execute(query, (value,))
-            cnx.commit()
-            self.send_json_response(200, {"status": "ok", "message": "Iterator value added successfully"})
-
-    def get_db_connection(self):
-        return mysql.connector.connect(user='root', password='root', host='172.10.0.10', database='iterator-db')
-
-    def send_json_response(self, status, data):
-        self.send_response(status)
+        self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf8'))
+        self.wfile.write(bytes(response, 'utf-8'))
 
-def run(server_class=HTTPServer, handler_class=MyHandler):
+    def health_status(self):
+        return json.dumps({'status': 'OK'})
+
+    def get_iterator_status(self):
+        db_connection = mysql.connector.connect(user='root', password='root', host='172.10.0.10', database='iterator-db')
+        cursor = db_connection.cursor()
+
+        try:
+            cursor.execute("SELECT state FROM interator")
+            state = cursor.fetchone()[0]
+            return json.dumps({'status': 'OK', 'iterator': state})
+        except Exception as e:
+            return json.dumps({'status': 'Error', 'message': str(e)})
+        finally:
+            cursor.close()
+            db_connection.close()
+
+    def add_iterator(self, data):
+        try:
+            value = int(data.get('value'))
+        except (ValueError, TypeError):
+            return json.dumps({'status': 'Error', 'message': 'Invalid input'})
+
+        db_connection = mysql.connector.connect(user='root', password='root', host='172.10.0.10', database='iterator-db')
+        cursor = db_connection.cursor()
+
+        try:
+            cursor.execute("UPDATE iterator SET state = %s", (value,))
+            db_connection.commit()
+            return json.dumps({'status': 'OK', 'message': 'Iterator value updated successfully'})
+        except Exception as e:
+            return json.dumps({'status': 'Error', 'message': str(e)})
+        finally:
+            cursor.close()
+            db_connection.close()
+
+if __name__ == '__main__':
     server_address = ('', 8080)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    httpd = TCPServer(server_address, MyHandler)
 
-if __name__ == "__main__":
-    run()
+    print('Serveur démarré sur le port 8080...')
+    httpd.serve_forever()
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-docker network rm intNetwork || true
+
 docker network create --subnet=172.10.0.0/16 intNetwork
-cd docker/mysql && docker run -d --name wildfly1 --ip 172.10.0.10 -h wildfly1 -p 3306:3306 --network=intNetwork mon_image_mysql
-cd ../python && docker run -d --name wildfly2 --ip 172.10.0.20 -h wildfly2 -p 8080:8080 --network=intNetwork my_python_app
+docker run -d --name mysql --ip 172.10.0.10 -p 3306:3306 --network=intNetwork mon_image_mysql
+docker run -d --name python --ip 172.10.0.20  -p 8000:8080 --network=intNetwork mon_image_python
+docker run -d --name  nginx --ip 172.10.0.30 -p 80:80   --network=intNetwork mon_image_nginx
+docker run --name newman  --network=intNetwork mon_image_newman
