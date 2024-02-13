dead226821ae345d522fc2b1ded7c5e757976103
Nalvac <vacna1997@gmail.com>
2024-02-13 08:35:24 +0100

    New conf added

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 docker network create --subnet=172.10.0.0/16 intNetwork
 docker run -d --name mysql --ip 172.10.0.10 -p 3306:3306 --network=intNetwork mon_image_mysql
 docker run -d --name python --ip 172.10.0.20  -p 8000:8080 --network=intNetwork mon_image_python
-docker run -d --name  nginx --ip 172.10.0.30 -p 80:80   --network=intNetwork mon_image_nginx
-docker run --name newman  --network=intNetwork mon_image_newman
+docker run -d --name nginx --ip 172.10.0.30 -p 80:80 --network=intNetwork mon_image_nginx
+docker run --name newman --ip 172.10.0.40 -p 6000:6000 --network=intNetwork mon_image_newman
